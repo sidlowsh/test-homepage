8a5ddceb114a7b216a07e98b24021d3fee171c48
sidlowsh <sarah.sidlow@georgetown.edu>
2019-03-17 22:49:15 -0400

    Add percentage function

--- homepage.php
+++ homepage.php
@@ -1,7 +1,14 @@
-	<?php 
-  
+<?php   
 	echo "Today's date is :"; 
 	$today = date("d/m/Y"); 
 	echo $today; 
-	  
-	?> };
+?> 
+
+<?php 
+	function getPercentage($number, $total, $percentage){
+	$percentage=($number*100)/$total;	
+	return $percentage;
+	}
+	getPercentage (number: 3, total: 21);
+?>
+
