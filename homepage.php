35b4a5ebf00ce70d60126fb1dc34d392760ec0ca
sidlowsh <sarah.sidlow@georgetown.edu>
2019-03-17 22:57:59 -0400

    Add phone number

--- homepage.php
+++ homepage.php
@@ -1,10 +1,10 @@
-<?php   
+<? php   
 	echo "Today's date is :"; 
 	$today = date("d/m/Y"); 
 	echo $today; 
 ?> 
 
-<?php 
+<? php 
 	function getPercentage($number, $total, $percentage){
 	$percentage=($number*100)/$total;	
 	return $percentage;
@@ -12,3 +12,11 @@
 	getPercentage (number: 3, total: 21);
 ?>
 
+<? php
+	$phone_numbers = [
+	  "Sarah" => "867-5309",
+	];
+
+	print_r($phone_numbers);
+	echo "Sarah's phone number is " . $phone_numbers["Sarah"] . "\n";
+?>
